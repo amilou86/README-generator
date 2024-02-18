439a0fdb86e75c656e8090fc984aaac0d1017c31
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-02-18 11:48:31 +0000

    license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,32 @@
+
+const options = require("../index.js");
+
+// get license badge
+function licenseBadge(license) {
+  if (license === options.Apache) {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if (license === options.GNU) {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if (license === options.MIT) {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } 
+  else if (license === options.Boost) {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  } 
+  else if (license === options.Eclipse) {
+    return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+  } 
+  else if (license === options.Mozilla) {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    console.log(license);
+  }
+};
+
+
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
