1bb11b377102954dc700c6762c0d6cc1a4b17b2d
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-02-18 12:13:08 +0000

    README sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,13 +11,13 @@ function licenseBadge(license) {
   }
   else if (license === options.MIT) {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-  } 
+  }
   else if (license === options.Boost) {
     return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
-  } 
+  }
   else if (license === options.Eclipse) {
     return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
-  } 
+  }
   else if (license === options.Mozilla) {
     return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
     console.log(license);
@@ -29,18 +29,20 @@ function generateMarkdown(data) {
   return `# ${data.title}
 ${licenseBadge(data.license)}
 ## Description
-
+${data.descrition}
 ## table of Contents //list of sections in the README with links to each
 
 ## Installation
-
+${data.dependencies}
 ## Usage
-
+${date.usage}
 ## Contributions
-
+${data.contributors}
 ## Tests
-
+${data.test}
 ## Questions // github username & email address added here with link to profile
+Please send any questions to;
+${[data.name], [data.email]}
 `;
 }
 
